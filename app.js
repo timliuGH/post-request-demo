3fee4c7a2fd26b23a8af4ca448476ff786f438a3
timliuGH <timsgithub@gmail.com>
2019-06-29 04:09:22 +0000

    Add new friend to list of friends

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 8080;
 const ip = "0.0.0.0";
 
 console.log(process.env.IP);
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+var friends = ['Luffy', 'Zoro', 'Nami', 'Usopp', 'Sanji', 'Chopper', 'Robin', 'Franky'];
 
 app.get('/', function(req, res) {
     res.render('home');
 });
 
 app.get('/friends', function(req, res) {
-    var friends = ['Luffy', 'Zoro', 'Nami', 'Usopp', 'Sanji', 'Chopper', 'Robin', 'Franky'];
     res.render('friends', {friends: friends});
 });
 
 app.post('/addFriend', function(req, res) {
-    res.send('You have reached the post route');
+    let newFriend = req.body.newFriend;
+    friends.push(newFriend);
+    res.redirect('/friends');
 });
 
 app.listen(port, ip, function() {
